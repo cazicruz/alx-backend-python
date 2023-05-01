846229869c6cdd4cab0221ad4f1a2ecbbe33daed
david onwuli <dcazicruz@icloud.com>
2023-05-01 14:54:40 +0100

    task 0

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,5 +2,7 @@
 """ type annotation in python"""
 
 
-def concat(str1: str,str2: str) ->str:
-    return(str1+str2)
+def concat(str1: str, str2: str) ->str:
+    """function to add srt1 and str2 to give a whole sentence
+    str1 and str2 have to be strings"""
+    return(str1 + str2)
