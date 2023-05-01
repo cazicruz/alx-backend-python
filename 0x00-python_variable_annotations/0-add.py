d1df1c08522c409371064a80e69fdbf29330a1d7
david onwuli <dcazicruz@icloud.com>
2023-05-01 14:37:20 +0100

    task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,5 +2,7 @@
 
 """python function annotations"""
 
-def add(a:float, b:float) -> float :
+
+def add(a: float, b: float) -> float:
+    """a function that adds two args (a and b)"""
     return(a + b)
