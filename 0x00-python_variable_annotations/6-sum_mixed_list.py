43e39a79ea6fbdd6cf2f221923f98d0e295d6136
david onwuli <dcazicruz@icloud.com>
2023-05-01 16:00:48 +0100

    task 0

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """function annotation"""
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: list[float || int]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """ this function retuns the sum of the list"""
     return float(sum(mxd_list))
