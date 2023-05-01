bc5f784225b0c797f06b58ddfed32b99d8f29559
david onwuli <dcazicruz@icloud.com>
2023-05-01 15:57:02 +0100

    task 0

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function annotation"""
+
+
+def sum_mixed_list(mxd_list: list[float || int]) -> float:
+    """ this function retuns the sum of the list"""
+    return float(sum(mxd_list))
