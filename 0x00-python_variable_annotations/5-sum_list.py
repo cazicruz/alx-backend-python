9dd1bfe82291a779960056e963e32727410f5be1
david onwuli <dcazicruz@icloud.com>
2023-05-01 15:35:36 +0100

    task 0

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function annotation"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """ this function retuns the sum of the list"""
+    return sum(input_list)
