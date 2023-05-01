f32b68d48edf2b383e30656829eb41e6c820956a
david onwuli <dcazicruz@icloud.com>
2023-05-01 17:11:59 +0100

    task 0

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,7 +3,7 @@
 from typing import Mapping, Any, Union
 
 
-def safely_get_value(dct: Mapping, key: Any, default:Union[~T, None] = None) -> Union[Any, ~T]:
+def safely_get_value(dct: Mapping, key: Any, default:Union[`T`, None] = None) -> Union[Any, `T`]:
     """returns the value of the dict key"""
     if key in dct:
         return dct[key]
