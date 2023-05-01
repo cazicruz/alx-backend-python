e2c8abdd92572602df8f211d9d6edf356355b94b
david onwuli <dcazicruz@icloud.com>
2023-05-01 16:12:50 +0100

    task 0

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""function annotation"""
+from typing import List
+
+
+def to_kv(k: str, v: Union[int. float]) -> Tuple:
+    """ this function retuns a tuple and it takes 
+    a string and a integer or float"""
+    x = (k, v*v)
+    return x
