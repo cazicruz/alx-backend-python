659e80fad9c8062d1dfb1d8797fb0b51c6bfe701
david onwuli <dcazicruz@icloud.com>
2023-05-01 16:57:30 +0100

    task 0

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Duck type annotation in python3"""
-from typing import Sequence, Any, Union, NoneType
+from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, Nonetype]:
