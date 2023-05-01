a5e7645d4719f6d77a17908a94b528c9930cbb5e
david onwuli <dcazicruz@icloud.com>
2023-05-01 15:04:07 +0100

    task 0

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import math
 """function annotation in python"""
+import math
 
 
 def floor(n: float) -> int:
-    """ returns n to the nearest whole number using 
+    """ returns n to the nearest whole number using
     the maths floor method"""
     return(math.floor(n))
