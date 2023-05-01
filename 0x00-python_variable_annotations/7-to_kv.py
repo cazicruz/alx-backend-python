23b6decf40f67ed4e697c2eb423abcd6c6014d0f
david onwuli <dcazicruz@icloud.com>
2023-05-01 16:17:51 +0100

    task 0

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
 from typing import List, Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ this function retuns a tuple and it takes 
     a string and a integer or float"""
     x = (k, v*v)
