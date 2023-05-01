eac53a1f2c676ecf66c80993f753c69a9069b64d
david onwuli <dcazicruz@icloud.com>
2023-05-01 17:08:38 +0100

    task 0

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """annotation in python"""
-from typing import Mapping, Any, Union, ~T
+from typing import Mapping, Any, Union
 
 
 def safely_get_value(dct: Mapping, key: Any, default:Union[~T, None] = None) -> Union[Any, ~T]:
