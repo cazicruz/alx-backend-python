82d8f834c280acd782d213abfcaeed34a1fa5e42
david onwuli <dcazicruz@icloud.com>
2023-05-01 15:46:00 +0100

    task 0

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,4 +5,4 @@ from typing import List
 
 def sum_list(input_list: List[float]) -> float:
     """ this function retuns the sum of the list"""
-    return float(sum(input_list)i)
+    return sum(input_list)
