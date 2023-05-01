411de8b34a9463bf21cb97b1dbe1ce202709392a
david onwuli <dcazicruz@icloud.com>
2023-05-01 16:36:38 +0100

    task 0

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,6 +4,8 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """takes a value and returns a function (mul) that takes another value
+    and returns the multiplication of both values"""
     def mul(num: float) -> float:
         return(num * multiplier)
     return (mul)
