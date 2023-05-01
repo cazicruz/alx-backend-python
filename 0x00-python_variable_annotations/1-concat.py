1876675150b780fb6fcd5058fe0d4a0fd6bd35a0
david onwuli <dcazicruz@icloud.com>
2023-05-01 15:01:00 +0100

    task 0

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,7 +2,7 @@
 """ type annotation in python"""
 
 
-def concat(str1: str, str2: str) ->str:
+def concat(str1: str, str2: str) -> str:
     """function to add srt1 and str2 to give a whole sentence
     str1 and str2 have to be strings"""
     return(str1 + str2)
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import math
+"""function annotation in python"""
+
+
+def floor(n: float) -> int:
+    """ returns n to the nearest whole number"""
+    return(math.floor(n))
