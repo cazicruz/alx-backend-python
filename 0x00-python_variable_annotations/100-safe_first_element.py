c72141d96f4b5dff9d19c7d732628dee91a5ced6
david onwuli <dcazicruz@icloud.com>
2023-05-01 16:56:29 +0100

    task 0

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Duck type annotation in python3"""
+from typing import Sequence, Any, Union, NoneType
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, Nonetype]:
+    """Returns the first element in lst"""
+    if lst:
+        return lst[0]
+    else:
+        return None
