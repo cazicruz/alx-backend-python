46c060ae21c7bc4cfa9fd1e436edae2c075793e7
david onwuli <dcazicruz@icloud.com>
2023-05-01 16:33:59 +0100

    task 0

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -3,7 +3,7 @@
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     def mul(num: float) -> float:
         return(num * multiplier)
     return (mul)
