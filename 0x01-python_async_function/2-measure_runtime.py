03dc9f5630d34b108707554ffaf2d545b85515d2
david onwuli <dcazicruz@icloud.com>
2023-05-02 22:31:12 +0100

    task 0

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" function calculates the time of execution of wait_n"""
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """ calculates the time of execution of wait_n
+    with the  help of time.perf_counter
+    which gives us the start time and end time"""
+    s = time.perf_counter()
+    wait_n(n, max_delay)
+    execution_time = time.pref_counter() - s
+    return execution_time
