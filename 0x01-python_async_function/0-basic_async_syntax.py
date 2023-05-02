06930d1575630b5a448cf8dc28cc802220de8c4c
david onwuli <dcazicruz@icloud.com>
2023-05-02 19:12:02 +0100

    task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env puthon3
 """practicing asynchronous coroutine"""
 import random
+import asyncio
 
 
 async def wait_random(max_delay: int =10):
     """waits for a randome delay and returns the param"""
-    await.sleep(random.uniform(0, max_delay))
+    await asyncio.sleep(random.uniform(0, max_delay))
     return max_delay
