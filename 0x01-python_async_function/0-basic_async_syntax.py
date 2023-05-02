8d1139c55320e721666c205e7193e13cae188693
david onwuli <dcazicruz@icloud.com>
2023-05-02 19:15:02 +0100

    task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,3 +8,5 @@ async def wait_random(max_delay: int =10):
     """waits for a randome delay and returns the param"""
     await asyncio.sleep(random.uniform(0, max_delay))
     return max_delay
+
+asyncio.run(wait_random())
