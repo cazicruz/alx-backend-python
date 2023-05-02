a4a63cc542b3494dd2de38d9b9377e80013d4eb5
david onwuli <dcazicruz@icloud.com>
2023-05-02 22:50:12 +0100

    task 0

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""re-creating wait_n"""
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int , max_delay: int) -> List[float]:
+    """waits for task_wait_random function to execute n
+    times and return a list of its return value"""
+    task = [task_wait_random for _ in range(n)]
+    return[await task for task in task]
