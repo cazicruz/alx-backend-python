1b669e61cc99b5addfca16e710ce3f8436344cca
david onwuli <dcazicruz@icloud.com>
2023-05-02 22:13:47 +0100

    task 0

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""wait_n is a coroutines function that creates a list of delays"""
+import asyncio
+import random
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int , max_delay: int) -> list[float]:
+    """waits for wait_random function to execute n
+    times and return a list of its return value"""
+    task = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return[await task for task in task]
