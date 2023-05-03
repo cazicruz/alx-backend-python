a3062c3751e306b9248bc0175a6b2a1d7b13e100
david onwuli <dcazicruz@icloud.com>
2023-05-03 17:41:18 +0100

    task 0

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """async generator func"""
-from typing import Generator
-import asyncio, random
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[int, None, None]:
+async def async_comprehension() -> List[float]:
     """ returns 10 random value from async_generator"""
     return[await for i in async_generator()]
