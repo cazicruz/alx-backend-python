2b3d8b9e5ef18443e92e7c99dd085628e270536f
david onwuli <dcazicruz@icloud.com>
2023-05-03 17:20:30 +0100

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""async generator func"""
+from typing import Generator
+import asyncio
+import random
+
+
+async def async_generator() -> Generator[int, None, None]:
+    """ the function would loop 10x and yeild a random value"""
+    i = 0
+    while i < 10:
+        await asyncio.sleep(1)
+        yield(random.uniform(0,10))
