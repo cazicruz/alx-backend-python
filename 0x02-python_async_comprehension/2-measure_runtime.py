bb58591c317c02a2cfbf40b825a36673751b8d8d
david onwuli <dcazicruz@icloud.com>
2023-05-03 17:58:49 +0100

    task 0

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""time calculation code for an async func"""
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ returns the time it takes to execute async_comprehension 4x"""
+    s = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+            async_comprehension())
+    runtime = time.perf_counter() - s
+    return runtime
