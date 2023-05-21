6a9af920446e68d98e9a5d6f0fd6239fdf60cb9e
david onwuli <dcazicruz@icloud.com>
2023-05-21 21:17:10 +0100

    task0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,12 @@ class TestAccessNestedMap(unittest.TestCase):
                                     path: Sequence, expected: Any) -> None:
         """test the access_nested_map method"""
         self.assertEqual(access_nested_map(nested_map, b), expected)
+
+    @parameterized.expand([
+        ({},("a",)),
+        ({"a": 1}, ("a", "b"))
+        ])
+    def test_access_nested_map_exception(self, nested_map:Mapping,
+                                            path:Sequence): -> None
+        self.assertRaise(KeyError):
+            access_nested_map(nested_map, path)
